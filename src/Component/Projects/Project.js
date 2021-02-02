7b1fd91243a7b853c6c7c377e75cb852a1ffab9b
TL <TL@gmail.com>
2021-02-02 21:02:17 +0545

    link updated

--- src/Component/Projects/Project.js
+++ src/Component/Projects/Project.js
@@ -10,7 +10,7 @@ const Project = () => {
       <h1>Projects(2019-2020)</h1>
   <a href='https://firsttodoapp.imfast.io/ToDo_App-master/'>▶️ TODO List (JavaScript)</a>
   <a href='https://budgetcalculator.imfast.io/BudgeCalculator-master/'>▶️ Budget Calculator (JavaScript)</a>
-  <a href='https://mapapppp.imfast.io/'>▶️ Google Map (JavaScript)</a>
+  <a href='https://bipinparajuli.github.io/GoogleMap/'>▶️ Google Map (JavaScript)</a>
   <a href='https://todo-app-cp-e2296.web.app/'>▶️ Amazone Clone (JavaScript,React,Firebase)</a>
   <a href="https://kinbechh.netlify.app/">▶️ E-Commerce (JavaScript,React,Firebase)</a>
   <a href="https://mobile-stores.netlify.app/">▶️Full-Stack E-Commerce (MERN)</a>
