81052f682240890913ba303f4016c0ecf05a7db9
bipinparajuli <bipinprjl@gmail.com>
2021-05-14 22:10:49 +0545

    meta content added

--- src/Component/ImageContainer/ImageContainer.js
+++ src/Component/ImageContainer/ImageContainer.js
@@ -5,7 +5,7 @@ import img from '../../Assets/Images/a.jpg'
 const ImageContainer = () => {
     return (
     <div>
-            <Image style={{borderRadius:"50%",height:"150px",width:"150px",margin:'10px 0 0 15px'}} src={img} roundedCircle />
+            <Image alt="parajuli" style={{borderRadius:"50%",height:"150px",width:"150px",margin:'10px 0 0 15px'}} src={img} roundedCircle />
     </div>
     )
 }
